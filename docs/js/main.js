550acae9606046ef81cb894695e8bb8f50ad3f52
Igor Silakov <igor.silakov85@gmail.com>
2019-11-26 02:34:16 +0300

    add markup for nav & search, style search, add logic for layouts switch

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,27 @@
-console.log('js will be here')
+const changeLayout = function(mod1, mod2) {
+  let curMod = mod1;
+  let newMod = mod2;
+  let elements = document.querySelectorAll(`[class*="${curMod}"]`);
+  const switchMod = function() {
+    let temp = curMod;
+    curMod = newMod;
+    newMod = temp;
+  };
+  if (elements.length === 0) {
+    switchMod();
+    elements = document.querySelectorAll(`[class*="${curMod}"]`);
+  }
+  elements.forEach(item => {
+    item.className.split(/\s+/).forEach(name => {
+      if(name.includes(curMod)) {
+        let newName = name.replace(curMod, newMod);
+        item.classList.toggle(name);
+        item.classList.toggle(newName);
+      }
+    })
+  })
+  switchMod();
+};
+document.querySelector('.switch-layout').onclick = () => {
+  changeLayout('--bruno', '--levi');
+};
